bab42fa0a8f61467d0d8c01fc61d444647de9acd
RainOwl <academic_ymj@163.com>
2018-02-07 23:35:18 +0800

    remark running
    remark run the celery

--- tasks.py
+++ tasks.py
@@ -17,3 +17,6 @@ def run_cmd(cmd):
     time.sleep(10)
     cmd_obj = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     return json.dumps(cmd_obj.stdout.read().decode('utf8'))
+
+# 运行 celery_study$ celery -A tasks worker -l debug
+# tasks.add.delay(2,33)
